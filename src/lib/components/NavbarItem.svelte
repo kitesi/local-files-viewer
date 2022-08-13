7b16ef6366423d5c6085614818452fcb9497e918
karizma <karizma7@protonmail.com>
2022-08-12 21:32:31 -0400

    add icons

--- src/lib/components/NavbarItem.svelte
+++ src/lib/components/NavbarItem.svelte
@@ -1,33 +1,4 @@
-<script lang="ts">
-	import type { WalkDirItem } from 'src/mem-fs';
-	// import Icon from './Icon.svelte';
-	import { page } from '$app/stores';
-
-	export let name: string;
-	export let children: WalkDirItem[] | undefined;
-	export let basePath: string;
-
-	const hasChildren = children && children.length > 0;
-	const href = basePath + '/' + name;
-	const isActive = '/' + $page.params.file === href;
-	let shouldCollapse = true;
-
-	const filesInPageParam = $page.params.file.split('/');
-	const filesInHref = href.split('/').slice(1);
-
-	// prob a better/faster way to do this
-	// if a folder has already been collapsed, obv it's siblings can't be collapsed
-	// originally had $page.params.file.split('/').includes(name) but that's in accurate
-	for (
-		let i = 0;
-		i < filesInHref.length && i < filesInPageParam.length && shouldCollapse;
-		i++
-	) {
-		if (filesInHref[i] !== filesInPageParam[i]) {
-			shouldCollapse = false;
-		}
-	}
-
+<script context="module" lang="ts">
 	let activeTag: HTMLAnchorElement;
 
 	function handleClick(ev: Event) {
@@ -48,11 +19,43 @@
 		a.classList.add('active');
 		activeTag = a;
 
-		if (!hasChildren) {
+		if (!a.parentElement?.classList.contains('has-children')) {
 			return;
 		}
 
 		a.parentElement!.classList.toggle('collapse');
+	}
+</script>
+
+<script lang="ts">
+	import type { WalkDirItem } from 'src/mem-fs';
+	import Icon from './Icon.svelte';
+	import { page } from '$app/stores';
+
+	export let name: string;
+	export let children: WalkDirItem[] | undefined;
+	export let basePath: string;
+
+	const hasChildren = children && children.length > 0;
+	const href = basePath + '/' + name;
+	const isActive = '/' + $page.params.file === href;
+	let shouldCollapse = true;
+	console.log(href, $page.params.file);
+
+	const filesInPageParam = $page.params.file.split('/');
+	const filesInHref = href.split('/').slice(1);
+
+	// prob a better/faster way to do this
+	// if a folder has already been collapsed, obv it's siblings can't be collapsed
+	// originally had $page.params.file.split('/').includes(name) but that's in accurate
+	for (
+		let i = 0;
+		i < filesInHref.length && i < filesInPageParam.length && shouldCollapse;
+		i++
+	) {
+		if (filesInHref[i] !== filesInPageParam[i]) {
+			shouldCollapse = false;
+		}
 	}
 </script>
 
@@ -61,7 +64,10 @@
 	class:active={isActive}
 	class:collapse={shouldCollapse}
 >
-	<a on:click={handleClick} {href}> {name}</a>
+	<a on:click={handleClick} {href}>
+		<Icon name={hasChildren ? 'folder' : 'file'} />
+		{name}</a
+	>
 	{#if children}
 		<ul>
 			{#each children as child (child.name)}
